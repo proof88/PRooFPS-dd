eaa551d85dabb2333b505a5fa370cf0c1332deac
proof88 <pr0o0o0o0o0o0o0o0o0o0of88@gmail.com>
2024-05-19 00:33:23 +0200

    CR: Player Frags are Decremented when Falling Off the Map

--- PRooFPS-dd/Physics.cpp
+++ PRooFPS-dd/Physics.cpp
@@ -350,6 +350,7 @@ void proofps_dd::Physics::serverGravity(
             // In the future physics need to handle fall damage too, but that could also just set health to 0.
             // Note that we also need to consider who will know that upon dieing by falling down, splash die sound is needed to be played.
             // Because that would be also sent to clients so they can also play the sound. Maybe set death reason, I dont know.
+            player.getFrags()--; // suicide
             handlePlayerDied(player, objXHair, player.getServerSideConnectionHandle(), gameMode);
 
             //if (player.isFalling())
