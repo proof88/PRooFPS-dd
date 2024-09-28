3c763fa5c3094016c7a6e2fa3d5c895f02fefa9d
proof88 <pr0o0o0o0o0o0o0o0o0o0of88@gmail.com>
2024-09-29 01:51:09 +0200

    Remove Deprecated Code

--- PRooFPS-dd/PRooFPS-dd-PGE.cpp
+++ PRooFPS-dd/PRooFPS-dd-PGE.cpp
@@ -236,9 +236,6 @@ bool proofps_dd::PRooFPSddPGE::onGameInitialized()
     getAudio().loadSound(m_sounds.m_sndMenuMusic,     std::string(proofps_dd::GAME_AUDIO_DIR) + "menu/Monkeys_Spinning_Monkeys.mp3");
     m_sounds.m_sndMenuMusic.setSingleInstance(true);
     getAudio().loadSound(m_sounds.m_sndLetsgo,        std::string(proofps_dd::GAME_AUDIO_DIR) + "radio/locknload.wav");
-    getAudio().loadSound(m_sounds.m_sndReloadStart,   std::string(proofps_dd::GAME_AUDIO_DIR) + "weapons/de_clipout.wav");
-    getAudio().loadSound(m_sounds.m_sndReloadFinish,  std::string(proofps_dd::GAME_AUDIO_DIR) + "weapons/de_clipin.wav");
-    getAudio().loadSound(m_sounds.m_sndReloadBazooka, std::string(proofps_dd::GAME_AUDIO_DIR) + "weapons/bazooka_reload.wav");
     getAudio().loadSound(m_sounds.m_sndChangeWeapon,  std::string(proofps_dd::GAME_AUDIO_DIR) + "weapons/m4a1_deploy.wav");
     getAudio().loadSound(m_sounds.m_sndPlayerDie,     std::string(proofps_dd::GAME_AUDIO_DIR) + "radio/die1.wav");
     getAudio().loadSound(m_sounds.m_sndExplosion,     std::string(proofps_dd::GAME_AUDIO_DIR) + "weapons/xplosion.wav");
--- PRooFPS-dd/Sounds.h
+++ PRooFPS-dd/Sounds.h
@@ -36,10 +36,6 @@ namespace proofps_dd
         SoLoud::Wav m_sndMenuMusic;
         SoLoud::handle m_sndMenuMusicHandle{};
         SoLoud::Wav m_sndLetsgo;
-        SoLoud::Wav m_sndReloadStart;
-        SoLoud::Wav m_sndReloadFinish;
-        SoLoud::Wav m_sndShootPistol;
-        SoLoud::Wav m_sndReloadBazooka;
         SoLoud::Wav m_sndChangeWeapon;
         SoLoud::Wav m_sndPlayerDie;
         SoLoud::Wav m_sndExplosion;
