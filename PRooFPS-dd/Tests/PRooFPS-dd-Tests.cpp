854ed6f1e511aec3935948ae507645616a98f439
proof88 <pr0o0o0o0o0o0o0o0o0o0of88@gmail.com>
2024-09-30 00:19:11 +0200

    Update Packet Rate Calculations

--- PRooFPS-dd/Tests/PRooFPS-dd-Tests.cpp
+++ PRooFPS-dd/Tests/PRooFPS-dd-Tests.cpp
@@ -83,10 +83,10 @@ int WINAPI WinMain(const _In_ HINSTANCE /*hInstance*/, const _In_opt_ HINSTANCE 
     tests.push_back(std::unique_ptr<UnitTest>(new RegTestBasicServerClient2Players(60, 60, 60)));
     tests.push_back(std::unique_ptr<UnitTest>(new RegTestBasicServerClient2Players(60, 20, 60)));
     tests.push_back(std::unique_ptr<UnitTest>(new RegTestBasicServerClient2Players(20, 20, 60)));
-    constexpr bool bAreWeTestingReleaseBuild = false;
-    tests.push_back(std::unique_ptr<UnitTest>(
-        new RegTestMapChangeServerClient3Players(60, 60, 60, 3 /*iterations*/, bAreWeTestingReleaseBuild, 2 /*clients*/)
-    ));
+    //constexpr bool bAreWeTestingReleaseBuild = true;
+    //tests.push_back(std::unique_ptr<UnitTest>(
+    //    new RegTestMapChangeServerClient3Players(60, 60, 60, 3 /*iterations*/, bAreWeTestingReleaseBuild, 2 /*clients*/)
+    //));
 
     std::vector<std::unique_ptr<UnitTest>>::size_type nSucceededTests = 0;
     std::vector<std::unique_ptr<UnitTest>>::size_type nTotalSubTests = 0;
